aedb9568b20962cfc2ba4cc88db860a1d6755262
Samaneasyeat <saman@easyeat.ai>
2024-04-23 15:38:25 +0530

    "unshift and Pop method in an array"

--- DSAProblem3.js
+++ DSAProblem3.js
@@ -5,3 +5,7 @@ names.push("Divya"); //push is method to add elements in array
 console.log(names); //it will return this values ["Saman","Amaan","Zara","Fiza","Divya"];
 names.unshift("ami"); //unshift is method and it is same like push method but the difference is unshift add from start and push add from last 
 console.log(names); // it will return the value of array ["ami","Saman","Amaan","Zara","Fiza","Divya"];
+ let lastName = names.pop(); //it will remove Divya and return Divya as well
+ console.log(lastName);
+ console.log(names);
+
