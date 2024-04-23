98785eeb1cedae5f5df257defa27b15834438c41
Samaneasyeat <saman@easyeat.ai>
2024-04-23 15:30:55 +0530

    "push and unshift method for adding elements in array"

---
+++ DSAProblem3.js
@@ -0,0 +1,7 @@
+// adding a value in array
+let names = ["Saman","Amaan","Zara","Fiza"];
+console.log(names);
+names.push("Divya"); //push is method to add elements in array 
+console.log(names); //it will return this values ["Saman","Amaan","Zara","Fiza","Divya"];
+names.unshift("ami"); //unshift is method and it is same like push method but the difference is unshift add from start and push add from last 
+console.log(names); // it will return the value of array ["ami","Saman","Amaan","Zara","Fiza","Divya"];
