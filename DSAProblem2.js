a7dabbc8079102088e1a7cac3620d556f5dadc8d
Samaneasyeat <saman@easyeat.ai>
2024-04-23 15:01:40 +0530

    "constructor and use of new keyword "

---
+++ DSAProblem2.js
@@ -0,0 +1,5 @@
+//constructor in javaScript 
+let students = new Array(5); //we are using new constructor for making a array of 5 elements 
+console.log(students); // it will give empty list of 5 array elemets 
+students = ["Mohan", "Rakesh", "Akash","Vishnu"]; //we are updating the values of array 
+console.log(students); //it will give updated values of students;
