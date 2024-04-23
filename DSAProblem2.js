c01543c0f98ca2516d3af70d3381ba5474fc59c6
Samaneasyeat <saman@easyeat.ai>
2024-04-23 15:07:30 +0530

    accesing elements of an array

--- DSAProblem2.js
+++ DSAProblem2.js
@@ -3,3 +3,14 @@ let students = new Array(5); //we are using new constructor for making a array o
 console.log(students); // it will give empty list of 5 array elemets 
 students = ["Mohan", "Rakesh", "Akash","Vishnu"]; //we are updating the values of array 
 console.log(students); //it will give updated values of students;
+//Accessing Elements of an Array
+let fruits = ["Apple","Banana","Mango","grapes", "watermelon"];
+console.log(fruits[0]); //output would be Apple
+console.log(fruits[1]);//output would be Banana
+console.log(fruits[2]); //output would be Mango
+console.log(fruits[3]); //output would be Grapes
+console.log(fruits[4]); //output would be watermelon
+console.log(fruits[5]); //output would be undefined
+console.log(fruits[6]); //output would be undefined
+
+
