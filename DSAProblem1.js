9d09692a79dde25cf0854b946a44e667c1d33d05
Samaneasyeat <saman@easyeat.ai>
2024-04-23 14:34:28 +0530

    "more declaration of arrays "

--- DSAProblem1.js
+++ DSAProblem1.js
@@ -1,6 +1,7 @@
 let house = new Array(10,20,30,40,50);
-let house1 = new Array(5);
+// in this Array we are creating an array of 5 elements like 10,20,30,40,50
+let house1 = new Array(5); // in this Array we are initializing array of 5 empty elements
 console.log(house);
 console.log(house1);
-let house2 = new Array("BHK");
+let house2 = new Array("BHK"); // in this we are making an array of 1 item "BHK";
 console.log(house2);
