9afc1c97a8ff4416f96399676cf1078ed7ce5a70
Samaneasyeat <saman@easyeat.ai>
2024-04-24 13:05:32 +0530

    "typeOf function"

--- DSAProblem5.js
+++ DSAProblem5.js
@@ -7,3 +7,7 @@ console.log(item); //it will concatenate items of fruits with vegetables and wil
 let names = ["saman","zara","fiza"];
 let newarray = names.toString();
 console.log(newarray); // it will convert the array to string and will give result saman,zara,fiza
+//type of an array 
+let courses = ["DSA","ML"];
+let typeOfArray = typeof(courses);
+console.log(typeOfArray);
