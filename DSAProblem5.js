169255db60a55e7b0377252d1370a1e2fc2106c2
Samaneasyeat <saman@easyeat.ai>
2024-04-24 12:59:37 +0530

    "concatenate in arrays"

---
+++ DSAProblem5.js
@@ -0,0 +1,5 @@
+//concatenate in arrays
+let fruits =["anaar","mango"];
+let vegetables = ["chilli","cabbage"];
+let item = fruits.concat(vegetables);
+console.log(item); //it will concatenate items of fruits with vegetables and will return an array with items ['anaar','mango','chilli','cabbage'];
