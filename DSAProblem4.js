8acc8d30d572e3d4934319751df976f370c3e7bf
Samaneasyeat <saman@easyeat.ai>
2024-04-24 12:53:05 +0530

    "for each loop"

--- DSAProblem4.js
+++ DSAProblem4.js
@@ -3,3 +3,8 @@ let items = [21,4,5,6,7,8,9];
 for (let index = 0; index < items.length; index++) {
     console.log(items[index]);
 }
+//for each loop in javascript 
+let fruits = ["mango","grapes","anaar","lichi"];
+fruits.forEach(function myfunc(elements){
+    console.log(elements);
+})
