d01a52bdbf7824d30c560fbb340d2b3921d92557
Samaneasyeat <saman@easyeat.ai>
2024-04-25 14:21:50 +0530

    "min elements in array "

---
+++ DSAProblem7.js
@@ -0,0 +1,10 @@
+// let array = [78,5,6,4,3,50];
+// let min = array[0];
+// for (let index = 1; index < array.length; index++) {
+//     if(min>array[index]){
+//         min = array[index];
+//     }
+    
+// }
+// console.log(min);
+console.log("hello");
